dfc7bccc3d40bf9fa41e1985976523e3cfec3ead
kouryuu <antarticfreeze@gmail.com>
2016-08-31 20:16:27 -0500

    Voronoi Overlay in image.

--- glass-filter.js
+++ glass-filter.js
@@ -1,31 +1,36 @@
 var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d"),
+var image_canvas = document.getElementById("image");
+var ictx = image_canvas.getContext("2d");
+var img = document.getElementById("img");
+
+var vctx = canvas.getContext("2d"),
     width = canvas.width,
     height = canvas.height;
 var voronoi = d3.voronoi()
     .extent([[-1, -1], [width + 1, height + 1]]);
+vctx.drawImage(img,0,0);
 var sites = [];
 for(var i=0;i<200;i++){
   sites.push([Math.random(1) * width, Math.random(1) * height]);
 }
-context.fillStyle = "black";
+vctx.fillStyle = "black";
 sites.forEach(function(dots){
-  context.fillRect(dots[0],dots[1],5,5);
+  vctx.fillRect(dots[0],dots[1],5,5);
 
 });
 
 var diagram = voronoi(sites);
 var polygons = diagram.polygons();
 
-context.strokeStyle = "black";
+vctx.strokeStyle = "black";
 polygons.forEach(function(polygon){
-context.beginPath();
+vctx.beginPath();
   polygon.forEach(function(vertex){
-      context.lineTo(vertex[0],vertex[1]);
+      vctx.lineTo(vertex[0],vertex[1]);
     });
 
-  context.closePath();
- context.stroke();
+  vctx.closePath();
+ vctx.stroke();
  // context.fillStyle = "white";
   //context.fill();
 });
