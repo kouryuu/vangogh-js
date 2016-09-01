850a51fee958c0e3c70a0ff525f8b0087c5ca71c
kouryuu <antarticfreeze@gmail.com>
2016-08-31 21:15:41 -0500

    Works! With some color discrepancies.

--- glass-filter.js
+++ glass-filter.js
@@ -1,3 +1,4 @@
+window.onload = function(){
 var canvas = document.getElementById("canvas");
 var image_canvas = document.getElementById("image");
 var ictx = image_canvas.getContext("2d");
@@ -8,29 +9,48 @@ var vctx = canvas.getContext("2d"),
     height = canvas.height;
 var voronoi = d3.voronoi()
     .extent([[-1, -1], [width + 1, height + 1]]);
-vctx.drawImage(img,0,0);
+ictx.drawImage(img,0,0);
+img_data = ictx.getImageData(0,0,image_canvas.width,image_canvas.height);
 var sites = [];
-for(var i=0;i<200;i++){
-  sites.push([Math.random(1) * width, Math.random(1) * height]);
+for(var i=0;i<1000;i++){
+  sites.push([Math.random() * width, Math.random() * height]);
 }
 vctx.fillStyle = "black";
 sites.forEach(function(dots){
-  vctx.fillRect(dots[0],dots[1],5,5);
+  vctx.fillRect(dots[0],dots[1],2,2);
 
 });
 
 var diagram = voronoi(sites);
 var polygons = diagram.polygons();
 
-vctx.strokeStyle = "black";
-polygons.forEach(function(polygon){
+vctx.strokeStyle = "transparent";
+var polygonc = 0;
+polygons.forEach( function (polygon){
+point = sites[polygonc];
+
+colorToPaint = getPixelDataAt(Math.round(point[0]),Math.round(point[1]),image_canvas.width);
+color = "rgb("+colorToPaint[0]+","+colorToPaint[1]+","+colorToPaint[2]+")";
 vctx.beginPath();
   polygon.forEach(function(vertex){
       vctx.lineTo(vertex[0],vertex[1]);
     });
 
   vctx.closePath();
- vctx.stroke();
- // context.fillStyle = "white";
-  //context.fill();
+//  console.log(color);
+  vctx.strokeStyle = color;
+  vctx.fillStyle = color;
+  vctx.stroke();
+  vctx.fill();
+  polygonc++;
 });
+function getPixelDataAt(x,y,width){
+  r_index = (x*4)+(width*y*4);
+  g_index = r_index++;
+  b_index = g_index++;
+  r = img_data.data[r_index];
+  g = img_data.data[g_index];
+  b = img_data.data[b_index];
+  return [r,g,b];
+}
+}
